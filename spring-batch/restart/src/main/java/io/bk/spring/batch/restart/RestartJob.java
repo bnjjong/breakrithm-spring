d119e4c8f40d31a05b06ce872b30cd8a08899fd9
bnjjong <dogfootmaster@gmail.com>
2024-11-06 18:56:22 +0900

    Restart 배치잡 리팩토링.

--- spring-batch/restart/src/main/java/io/bk/spring/batch/restart/RestartJob.java
+++ spring-batch/restart/src/main/java/io/bk/spring/batch/restart/RestartJob.java
@@ -26,6 +26,7 @@ import org.springframework.batch.item.json.JacksonJsonObjectReader;
 import org.springframework.batch.item.json.JsonItemReader;
 import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,72 +37,73 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 @Slf4j
 public class RestartJob {
+
+  private static final int CHUNK_SIZE = 5;
   private final JobRepository jobRepository;
   private final PlatformTransactionManager transactionManager;
 
   @Bean(name = "helloJob")
-  public Job helloJob(Step step) {
+  public Job helloJob(@Qualifier("jsonToDatabaseStep") Step step) {
     return new JobBuilder("helloJob", jobRepository)
         .incrementer(new RunIdIncrementer())
         .start(step)
         .build();
   }
 
-    @Bean
-    public Step jsonToDatabaseStep(
-        JsonItemReader<UserDto> reader,
-        ItemWriter<UserDto> writer) {
-        return new StepBuilder("step", jobRepository)
-            .<UserDto, UserDto>chunk(5, transactionManager) // 청크 사이즈 설정
-            .reader(reader)
-            .writer(writer)
-            .listener(new StepExecutionListener() {
-                @Override
-                public void beforeStep(StepExecution stepExecution) {
-                    log.info("step before!!");
-                }
+  @Bean
+  public Step jsonToDatabaseStep(JsonItemReader<UserDto> reader, ItemWriter<UserDto> writer) {
+    return new StepBuilder("step", jobRepository)
+        .<UserDto, UserDto>chunk(CHUNK_SIZE, transactionManager) // 청크 사이즈 설정
+        .reader(reader)
+        .writer(writer)
+        .listener(
+            new StepExecutionListener() {
+              @Override
+              public void beforeStep(StepExecution stepExecution) {
+                log.info("step before!!");
+              }
 
-                @Override
-                public ExitStatus afterStep(StepExecution stepExecution) {
-                    log.info("index : {}", stepExecution.getReadCount());
-                    stepExecution.getExecutionContext().putLong("start.index", stepExecution.getReadCount());
-                    return ExitStatus.COMPLETED;
-                }
+              @Override
+              public ExitStatus afterStep(StepExecution stepExecution) {
+                log.info("index : {}", stepExecution.getReadCount());
+                stepExecution
+                    .getExecutionContext()
+                    .putLong("start.index", stepExecution.getReadCount());
+                return ExitStatus.COMPLETED;
+              }
             })
-            .listener(new ItemWriteListener<>() {
+        .listener(
+            new ItemWriteListener<>() {
 
-
-                @Override
-                public void afterWrite(Chunk<? extends UserDto> items) {
-                    ItemWriteListener.super.afterWrite(items);
-
-                }
+              @Override
+              public void afterWrite(Chunk<? extends UserDto> items) {
+                ItemWriteListener.super.afterWrite(items);
+              }
             })
-            .build();
-    }
+        .build();
+  }
 
-    @Bean
-    @StepScope
-    public JsonItemReader<UserDto> jsonItemReader(
-        @Value("#{stepExecutionContext['start.index']}") Long startIndex) {
+  @Bean
+  @StepScope
+  public JsonItemReader<UserDto> jsonItemReader(
+      @Value("#{stepExecutionContext['start.index']}") Long startIndex) {
 
-        return new JsonItemReaderBuilder<UserDto>()
-            .name("jsonItemReader")
-            .jsonObjectReader(new JacksonJsonObjectReader<>(UserDto.class))
-            .resource(new ClassPathResource("user.json"))
-            .saveState(true) //  현재 상태를 저장
-            .currentItemCount(startIndex != null ? startIndex.intValue() : 0)
-            .build();
-    }
+    return new JsonItemReaderBuilder<UserDto>()
+        .name("jsonItemReader")
+        .jsonObjectReader(new JacksonJsonObjectReader<>(UserDto.class))
+        .resource(new ClassPathResource("user.json"))
+        .saveState(true) //  현재 상태를 저장
+        .currentItemCount(startIndex != null ? startIndex.intValue() : 0)
+        .build();
+  }
 
-    @Bean
-    public ItemWriter<UserDto> writer() {
-        // JPA나 JDBC를 사용하여 데이터베이스에 쓰는 로직 구현
-        // 예: JpaItemWriter 또는 JdbcBatchItemWriter 사용
-        return items -> {
-            log.info("Writing {} items", items.size());
-            items.forEach(item -> log.info("Item: {}", item));
-        };
-
-    }
+  @Bean
+  public ItemWriter<UserDto> writer() {
+    // JPA나 JDBC를 사용하여 데이터베이스에 쓰는 로직 구현
+    // 예: JpaItemWriter 또는 JdbcBatchItemWriter 사용
+    return items -> {
+      log.info("Writing {} items", items.size());
+      items.forEach(item -> log.info("Item: {}", item));
+    };
+  }
 }
